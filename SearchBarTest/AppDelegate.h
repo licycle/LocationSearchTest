6991d6644cbaeabf5446577987f0dad189db47b7
admin <kangda@kangdademacbook-pro.local>
2015-12-15 19:03:34 +0800

    Initial Commit

---
+++ SearchBarTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SearchBarTest
+//
+//  Created by kangda on 15/12/15.
+//  Copyright © 2015年 kangda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SearchBarTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SearchBarTest
+//
+//  Created by kangda on 15/12/15.
+//  Copyright © 2015年 kangda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
